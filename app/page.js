4c2faec92c19ee8a732196c12ca196deba762a4e
Swarnalata Rana <swarnalata21@navgurukul.org>
2024-01-13 00:26:18 +0530

    thirtycommit

--- app/page.js
+++ app/page.js
@@ -1,64 +1,58 @@
 "use client"
-import "./globals.css";
-import Header from "./Header";
-import Footer from "./Footer";
 import React, { useState } from 'react';
-import {movie_datas} from "./data.js";
-import Movie_row from "./Movie_row";
+import { movie_datas } from './data.js';  
+import Header from './Header';
+import Footer from './Footer';
+import MovieRow from './Movie_row';
 
 const Page = () => {
-  const [movies, setMovies] = useState(movie_datas)
+  const [movies, setMovies] = useState(movie_datas);
   const [count, setCount] = useState({});
+
   const handleDeleteBtn = (movieId) => {
-    const updatedMovies = movies.filter(movie => movie.id !== movieId);
+    const updatedMovies = movies.filter((movie) => movie.id !== movieId);
     setMovies(updatedMovies);
   };
-  function likeBtn(id){
-  const updatevote={...count}
-    if (updatevote[id]== undefined){
-      updatevote[id]=1
-    }
-    else{
-      updatevote[id]+=1
-    }
-    setCount(updatevote)
-  }
 
-  function diclikeBtn(id){
-  const updatevote={...count}
-    if (updatevote[id]== undefined){
-      updatevote[id]=-1
-    }
-    else{
-      updatevote[id]-=1
-    }
-    setCount(updatevote)
-  }
-  const shortItem=movies.sort((a,b)=>{
-    const voteA=count[a.id] || 0;
-    const voteB=count[b.id] || 0;
-    return voteB-voteA;
-  })
+  const likeBtn = (id) => {
+    const updatevote = { ...count };
+    updatevote[id] = (updatevote[id] || 0) + 1;
+    setCount(updatevote);
+  };
+
+  const dislikeBtn = (id) => {
+    const updatevote = { ...count };
+    updatevote[id] = (updatevote[id] || 0) - 1;
+    setCount(updatevote);
+  };
+
+  const sortedMovies = movies.sort((a, b) => {
+    const voteA = count[a.id] || 0;
+    const voteB = count[b.id] || 0;
+    return voteB - voteA;
+  });
 
   return (
     <div>
-        <Header/>
-            {movies.map(movie => (
-                <Movie_row
-                    title={movie.title}
-                    desc={movie.desc}
-                    image={movie.img}
-                    year={movie.year}
-                    vote={count[movie.id]}
-                    onClickLike={()=>likeBtn(movie.id)}
-                    onClickDislike={()=>diclikeBtn(movie.id)}
-                    onDelete={() => handleDeleteBtn(movie.id)}
-                />               
-            ))}    
-        <Footer/>
-    </div>  
+      <Header />
+      {sortedMovies.map((movie) => (
+        <MovieRow
+          key={movie.id}  // Add a unique key prop
+          title={movie.title}
+          desc={movie.desc}
+          image={movie.img}
+          year={movie.year}
+          vote={count[movie.id] || 0}
+          onClickLike={() => likeBtn(movie.id)}
+          onClickDislike={() => dislikeBtn(movie.id)}
+          onDelete={() => handleDeleteBtn(movie.id)}
+        />
+      ))}
+      <Footer />
+    </div>
   );
 };
+
 export default Page;
 
  
